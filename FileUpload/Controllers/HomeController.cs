e7657bcad05696dbee44fe53c4a562b5b02d0bd1
abdulvahap selçuk <a.vahap_65.5@hotmail.com>
2021-07-24 19:17:16 +0300

    Add project files.

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var path = Path.Combine(Server.MapPath("~/UploadFiles"), file.FileName);
+                file.SaveAs(path);
+                TempData["result"] = "Güncelleme Başarılı.";
+            }
+
+            return View();
+        }
+
+    }
+}
